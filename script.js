b325e753d18730fe78a8544268cf37d24eb46d56
yulp76 <yulp@uchicago.edu>
2017-11-28 22:39:53 -0600

    reorganize

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var group = "income_group";
+var country = ["Vietnam","Brazil","Kenya","Nigeria"]
+
 var chartsDiv = document.getElementById("charts"),
     pageWidth = chartsDiv.clientWidth,
     pageHeight = chartsDiv.clientHeight,
@@ -5,7 +8,28 @@ var chartsDiv = document.getElementById("charts"),
     sankeyWidth = pageWidth * 7/12,
     mapWidth = pageWidth * 5/12,
     svg_h = 600,
-    color = d3.scaleOrdinal(d3.schemeCategory10);
+    R = Math.min(mapWidth, svg_h)/2,
+    donut_w = 25,
+    innerR = R - donut_w;
+
+var svg1 = d3.select("#chart1")
+           .append("svg")
+           .attr("width", sankeyWidth)
+           .attr("height", svg_h),
+    svg2 = d3.select("#chart2")
+           .append("svg")
+           .attr("width", mapWidth)
+           .attr("height", svg_h)
+           
+    svg2.append("clipPath")
+         .attr("id", "map_area")
+         .append("circle")
+         .attr("cx", mapWidth/2)
+         .attr("cy", svg_h/2)
+         .attr("r", innerR);
+
+var gMap = svg2.append("g")
+               .attr("clip-path", "url(#map_area)");
 
 
 var colorType = d3.scaleOrdinal()
@@ -28,35 +52,6 @@ var type = colorType.domain(),
     income = colorIncome.domain(),
     region = colorRegion.domain();
 
-var country = ["Vietnam","Brazil","Kenya","Nigeria"]
-
-var svg1 = d3.select("#chart1")
-           .append("svg")
-           .attr("width", sankeyWidth)
-           .attr("height", svg_h),
-    svg2 = d3.select("#chart2")
-           .append("svg")
-           .attr("width", mapWidth)
-           .attr("height", svg_h);
-
-// Read Data
-d3.queue()
-  .defer(d3.csv, 'data/sample.csv')
-  .defer(d3.json, 'data/world.json')
-  .awaitAll(load);
-
-function load(error, aid, world) {
-  if (error) { console.log(error); }
-
-  }
-
-// Visualization 1
-
-// Reference: https://bl.ocks.org/mbostock/ca9a0bb7ba204d12974bca90acc507c0
-
-//var group = "region"
-var group = "income_group";
-
 var sankey_layout = d3.sankey()
     .nodeId(function (d) {
         return d.name;      
@@ -65,16 +60,59 @@ var sankey_layout = d3.sankey()
     .nodePadding(20)
     .extent([[margin.left,margin.top], [sankeyWidth-50, svg_h-margin.bottom]]);
 
-d3.csv("data/sample.csv", function(error, data) {
-  if (error) { throw error; }
+var pie = d3.pie()
+            .value(function(d) { return d.value; });
 
-  graph = sankey_format(data, group);
-  console.log(graph);
-  plot_sankey(graph);
-  });
+var projection = d3.geoMercator()
+                  //.center([svg_w/2, svg_h/2])
+                  .translate([mapWidth/5, svg_h/3*2])
+                  .scale(250),
+    map_path = d3.geoPath()
+                 .projection(projection);
 
 
-function sankey_format(data, criteria) {
+d3.queue()
+  .defer(d3.csv, 'data/sample.csv')
+  .defer(d3.json, 'data/world.json')
+  .await(load);
+
+
+function load(error, aid, world) {
+  if (error) { console.log(error); }
+
+  //Sankey
+  graph = sankeyFormat(aid, group);
+  plotSankey(graph);
+
+  //Map
+  byType = aggregateType(aid); 
+  byCountry = aggregateCountry(aid);
+  byCountryTotal = byCountry[0];
+  byCountryType = byCountry[1];
+
+  centroid = getCentroid(world.features);
+
+  plotDonut(byType);  
+  drawMap(world.features);
+  drawCountryPie(centroid, byCountryType, byCountryTotal);
+  
+  }
+
+
+//Shared Functions
+function getColor(name) {
+      if (type.includes(name)) {
+        return colorType(name);
+      } else if (income.includes(name)) {
+        return colorIncome(name);
+      } else if (region.includes(name)) {
+        return colorRegion(name);
+      } else { return "#cccccc"; }
+};
+
+
+//Viz 1 Functions
+function sankeyFormat(data, criteria) {
 
   var nodes = [],
       graph = {"nodes":[], "links":[]};
@@ -112,17 +150,7 @@ function sankey_format(data, criteria) {
   return graph;
 }
 
-function getColor(name) {
-  if (type.includes(name)) {
-    return colorType(name);
-  } else if (income.includes(name)) {
-    return colorIncome(name);
-  } else if (region.includes(name)) {
-    return colorRegion(name);
-  } else { return "#cccccc"; }
-};
-
-function plot_sankey(graph) {
+function plotSankey(graph) {
   
   sankey_layout(graph);
 
@@ -188,7 +216,6 @@ function plot_sankey(graph) {
                       d3.select(this)
                         .attr("opacity", 1);
                       country = d3.select(this).attr("class").slice(8);
-                      console.log(country)
                       d3.selectAll(".link."+country)
                         .attr("stroke-opacity", 1);
                     })
@@ -235,112 +262,26 @@ function wrap(text, width) {
   });
 }
 
-// Visualization 2
 
-var g = svg2.append("g")
-            .attr("clip-path", "url(#map_area)");
 
-var R = Math.min(mapWidth, svg_h)/2,
-    donut_w = 40,
-    innerR = R - donut_w;
-
-svg2.append("clipPath")
-    .attr("id", "map_area")
-    .append("circle")
-    .attr("cx", mapWidth/2)
-    .attr("cy", svg_h/2)
-    .attr("r", innerR)
-
-function draw_arc(outerR, innerR) {
+//Viz 2 Functions
+function drawArc(outerR, innerR) {
   return d3.arc()
             .outerRadius(outerR)
             .innerRadius(innerR);
 };
 
-var pie = d3.pie()
-            .value(function(d) { return d.value; });
-
-var projection = d3.geoMercator()
-                  //.center([svg_w/2, svg_h/2])
-                  .translate([mapWidth/5, svg_h/3*2])
-                  .scale(250)
-
-var map_path = d3.geoPath()
-                 .projection(projection);
-
-
-d3.queue()
-  .defer(d3.csv, 'data/sample.csv')
-  .defer(d3.json, 'data/world.json')
-  .awaitAll(function (error, results) {
-    if (error) { throw error; }
-
-  byType = aggregate_by_type(results[0]);
-  donut_plot(byType);
-
-  draw_map(results[1].features);
-
-  var locate_country_centroid = d3.map();
-  results[1].features.forEach(function (country) {
-    locate_country_centroid.set(country.properties.name, map_path.centroid(country));
-    });
-
-  byCountry = aggregate_by_country(results[0]);
-  byCountryTotal = byCountry[0];
-  byCountryType = byCountry[1];
-
-  var points = g.selectAll("g.country_point")
-                  .data(byCountryType)
-                  .enter()
-                .append("g")
-                  .attr("class", "country_point")
-                  .attr("transform", function(d) { return "translate(" + locate_country_centroid.get(d.key)[0] + "," +
-                        locate_country_centroid.get(d.key)[1] +")";});
-
-  points.append("circle")
-          .attr("fill", "white")
-          .attr("r", 2)
-        .append("title")
-          .text(function(d) { return d.key });
-
-  var pies = points.selectAll(".country_pie")
-                      .data(function(d) { return pie(d.values); })
-                      .enter()
-                    .append("g")
-                      .attr("class", "country_pie");
-
-  var arc = draw_arc(function(d){ return Math.max(4, Math.sqrt(byCountryTotal.get(d.data.mainkey)/10000));}, 3);
-
-  pies.append("path")
-        .attr("d", arc)
-        .attr("fill", function(d) { console.log(d); return color(d.data.key); })
-      .append("title")
-        .text(function(d) { return d.data.key + ":\n"
-          + d3.format(".0%")(d.data.value/byCountryTotal.get(d.data.mainkey))
-          + " of US$" + d3.format(".2s")(byCountryTotal.get(d.data.mainkey));})
-
-});
-
-
-/* Reference: https://stackoverflow.com/questions/12062561/calculate-svg-path-centroid-with-d3-js
-
-function getBoundingBoxCenter (selection) {
-  var bbox = selection.node().getBBox();
-  console.log([bbox.x + bbox.width/2, bbox.y + bbox.height/2]);
-}
- */
-
-function aggregate_by_type(data) {
+function aggregateType(data) {
   return d3.nest()
   .key(function(d) { return d.dac_category_name; })
   .rollup(function(v) { return d3.sum(v, function(d) { return d.constant_amount; }); })
   .entries(data);
 };
 
-function aggregate_by_country(data) {
+function aggregateCountry(data) {
   byCountry = d3.nest().key(function(d) { return d.country_name; })
-                        .rollup(function(v) { return d3.sum(v, function(d) { return d.constant_amount; }); })
-                        .entries(data)
+                       .rollup(function(v) { return d3.sum(v, function(d) { return d.constant_amount; }); })
+                       .entries(data)
   var byCountryTotal = d3.map();
   byCountry.forEach(function(country) {
     byCountryTotal.set(country.key, country.value);
@@ -357,15 +298,24 @@ function aggregate_by_country(data) {
     })
   });
 
-  return [byCountryTotal, byCountryType] ;
-}
+  return [byCountryTotal, byCountryType];
+};
 
-function donut_plot(data) {
+function getCentroid(countries) {
+  var centroid = d3.map();
+  countries.forEach(function (country) {
+    centroid.set(country.properties.name, map_path.centroid(country));
+    });
+  return centroid;
+  };
 
-  var arc = draw_arc(R, innerR),
-      label = draw_arc(R-25, R-25);
 
-  var arcs = svg2.selectAll("g.arc")
+function plotDonut(data) {
+
+  var arc = drawArc(R, innerR),
+      label = drawArc(R-18, R-18);
+
+  var arcs = svg2.selectAll(".arc")
               .data(pie(data))
               .enter()
               .append("g")
@@ -373,7 +323,7 @@ function donut_plot(data) {
               .attr("transform", "translate(" + mapWidth/2 + "," + svg_h/2 + ")");
 
   arcs.append("path")
-        .attr("fill", function(d) { return color(d.data.key); })
+        .attr("fill", function(d) { return getColor(d.data.key); })
         .attr("d", arc)
         .append("title")
         .text(function(d) { return d.value});
@@ -382,33 +332,55 @@ function donut_plot(data) {
         .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")" +
           'rotate(' + ((d.startAngle + d.endAngle) / 2) * 180 / Math.PI + ')'; })
         .attr("text-anchor", "middle")
-        .attr("stroke", "white")
+        .attr("stroke", "black")
         .attr("font-family", "Candara")
         .text(function(d) { return d.data.key; });
 };
 
-function draw_map(dataset) {
-  var country = g.selectAll(".country")
-                  .data(dataset)
-                  .enter()
-                 .append("path")
-                  .attr("class", "country")
-                  .attr("d", map_path)
-                  .attr("fill", "#969696");
+
+function drawMap(dataset) {
+  var country = gMap.selectAll(".country")
+                  	.data(dataset)
+                  	.enter()
+                	 .append("path")
+                 	 .attr("class", "country")
+                 	 .attr("d", map_path)
+                 	 .attr("fill", "#969696");
 
   country.append("title")
           .text(function(d) { return d.properties.name });
-
-  /* 
-  var centroids = g.selectAll(".centroid")
-                     .data(dataset)
-                     .enter()
-                    .append("circle")
-                     .attr("class", "centroid")
-                     .attr("r", 2)
-                     .attr("cx", function(d) { return map_path.centroid(d)[0]; })
-                     .attr("cy", function(d) { return map_path.centroid(d)[1]; })
-                    .append("title")
-                     .text(function(d) { return d.properties.name });
-  */
+          
 };
+
+
+function drawCountryPie(centroid, byCountryType, byCountryTotal){
+  var points = gMap.selectAll(".centroid")
+                  .data(byCountryType)
+                  .enter()
+                .append("g")
+                  .attr("class", function(d) { return "centroid "+d.key; })
+                  .attr("transform", function(d) { return "translate(" + centroid.get(d.key)[0] + "," +
+                        centroid.get(d.key)[1] +")";});
+  
+  var pies = points.selectAll(".pie")
+                      .data(function(d) { return pie(d.values); })
+                      .enter()
+                    .append("g")
+                      .attr("class", "pie");
+  
+  points.append("circle")
+          .attr("fill", "white")
+          .attr("r", 2)
+        .append("title")
+          .text(function(d) { return d.key });
+
+  var arc = drawArc(function(d){ return Math.max(4, Math.sqrt(byCountryTotal.get(d.data.mainkey)/10000));}, 3);
+
+  pies.append("path")
+        .attr("d", arc)
+        .attr("fill", function(d) { return getColor(d.data.key); })
+      .append("title")
+        .text(function(d) { return d.data.key + ":\n"
+          + d3.format(".0%")(d.data.value/byCountryTotal.get(d.data.mainkey))
+          + " of US$" + d3.format(".2s")(byCountryTotal.get(d.data.mainkey));})
+  };
